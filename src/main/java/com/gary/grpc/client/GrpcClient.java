c4b7f2e9141c7d936600cd0a5f5d518e0d5e8407
Gary Ma <gary.ma@entelect.co.za>
2024-05-26 10:43:43 +0200

    finished client and server

---
+++ src/main/java/com/gary/grpc/client/GrpcClient.java
@@ -0,0 +1,32 @@
+package com.gary.grpc.client;
+
+import com.gary.grpc.HelloRequest;
+import com.gary.grpc.HelloResponse;
+import com.gary.grpc.HelloServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+/**
+ * This is the client class that will send a request to the server
+ * and receive a response back
+ * NB: The stub is the primary way for clients to interact with the server.
+ *
+ * Here we're using a blocking/synchronous stub so that the RPC call waits for the server to respond,
+ * and will either return a response or raise an exception.
+ */
+public class GrpcClient {
+    public static void main(String[] args) {
+        System.out.println("Hello from gRPC client");
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+                .usePlaintext()
+                .build();
+
+        HelloServiceGrpc.HelloServiceBlockingStub blockingStub = HelloServiceGrpc.newBlockingStub(channel);
+
+        HelloResponse response = blockingStub.hello(HelloRequest.newBuilder()
+                        .setFirstName("Gary")
+                        .setLastName("Ma")
+                        .build());
+        channel.shutdown();
+    }
+}
